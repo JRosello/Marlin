de45726208cdf98097163d53342af0bbbeae611c
Jordi <jordiroa@gmail.com>
2021-05-07 10:53:39 +0200

    Solverd errors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1705,17 +1705,18 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
-//#define PREHEAT_2_LABEL       "ABS"
-//#define PREHEAT_2_TEMP_HOTEND 240
-//#define PREHEAT_2_TEMP_BED    110
-//#define PREHEAT_2_TEMP_CHAMBER 35
-//#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 240
+#define PREHEAT_2_TEMP_BED     75
+#define PREHEAT_2_TEMP_CHAMBER 35
+#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_3_LABEL       "PETG"
-#define PREHEAT_3_TEMP_HOTEND 240
-#define PREHEAT_3_TEMP_BED     75
-#define PREHEAT_3_TEMP_CHAMBER 35
-#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
+
+//#define PREHEAT_3_LABEL       "ABS"
+//#define PREHEAT_2_TEMP_HOTEND 240
+//#define PREHEAT_3_TEMP_BED    110
+//#define PREHEAT_3_TEMP_CHAMBER 35
+//#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
 
 //#define PREHEAT_4_LABEL       "Nylon"
 //#define PREHEAT_4_TEMP_HOTEND 230
